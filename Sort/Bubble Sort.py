d83975b41e81836885a8e57a8f7edcec7761f704
Nehal Khan <128951002+Nehal-Khan-29@users.noreply.github.com>
2024-10-17 19:15:00 +0530

    Add files via upload

---
+++ Sort/Bubble Sort.py
@@ -0,0 +1,18 @@
+def bubble_sort(arr):
+    if not arr:
+        print("Array is Empty")
+    
+    else:    
+        for i in range(len(arr)):
+            for j in range(len(arr)-i-1):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+        print(arr)
+
+arr = [1,5,3,4,2]
+bubble_sort(arr)
+
+# Time Complexity 
+# •	Best case    : O(n)
+# •	Average case : O(n²)
+# •	Worst case   : O(n²)
